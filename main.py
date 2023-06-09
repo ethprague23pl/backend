06f8152f58350c6a3da9b89a72109b960619f419
ChinaPancake <mateusz.placek99@gmail.com>
2023-06-09 16:47:51 +0200

    second

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
-from fastapi import FastAPI, UploadFile
+from supabase import client
+
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
-from PIL import Image
 from supabase import create_client, Client
-from io import BytesIO
+from loguru import logger as LOG
+from datetime import datetime, time
+
+import requests as r
+
+from upload_on_ipfs import upload_file_on_ipfs
 from config import PROJECT_URL, PUBLIC_API, DATABASE_PSWD
+
+
 
 app = FastAPI()
 
 class Event(BaseModel):
+    LOG.info('cyce')
     name: str
     description: str
-    start_day: str
-    start_hour: str
-    finish_day: str
-    finish_hour: str
-    preview_image: str
-    event_image: str
+    start_day: datetime
+    start_hour: time
+    finish_day: datetime
+    finish_hour: time
 
-@app.post("/create-event")
-async def create_event(event: Event, preview_image: UploadFile = None, event_image: UploadFile = None):
-    # Access the event properties
-    event_name = event.name
-    event_description = event.description
-    start_day = event.start_day
-    start_hour = event.start_hour
-    finish_day = event.finish_day
-    finish_hour = event.finish_hour
-    preview_image = event.preview_image
-    event_image = event.event_image
+def update_db(event: Event, preview_image_url, event_image_url):
+    LOG.info('dupa')
+    url: str = PROJECT_URL
+    key: str = PUBLIC_API
+    supabase: Client = create_client(url, key)  
 
-    # Save preview_image as PNG
-    if preview_image is not None:
-        image_data = await preview_image.read()
-        image = Image.open(BytesIO(image_data))
-        preview_image_path = f"preview_{event_name}.png"
-        image.save(preview_image_path, "PNG")
+    supabase.table("events").insert({
+  "name": event.name,
+  "description": event.description,
+  "start_day": event.start_day,
+  "start_hour": event.start_hour,
+  "finish_day": event.finish_day,
+  "finish_hour": event.finish_hour,
+  "preview_image": preview_image_url,
+  "event_image": event_image_url}).execute()
+    return {'Msg':'Created good'}
 
-    # Save event_image as PNG
-    if event_image is not None:
-        image_data = await event_image.read()
-        image = Image.open(BytesIO(image_data))
-        event_image_path = f"event_{event_name}.png"
-        image.save(event_image_path, "PNG")
 
-    return {"message": "Event created successfully"}
+@app.post("/create_event")
+async def create_event(event: Event):
+    return {'Git':"Good"}
 
+@app.post("/upload_image")
+def create_picture(first_art: UploadFile = File(...)):
+    """Takes image file, returns url to IPFS Storage"""
+
+    LOG.info(f"New NFT was initiated")
+
+    ipfs_url = upload_file_on_ipfs(first_art.file)
+
+    LOG.info(f"IPFS NTF url: {ipfs_url}")
+
+    return {"url": ipfs_url}
---
+++ upload_on_ipfs.py
@@ -0,0 +1,31 @@
+import requests as r
+from io import BytesIO
+from loguru import logger as LOG
+from PIL import Image
+from config import NFT_STORAGE_API
+
+
+def upload_file_on_ipfs(file):
+    api_key: str = NFT_STORAGE_API
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+    }
+
+    LOG.info("Uploading image to NFTStorage")
+
+    resp = r.post(
+        "https://api.nft.storage/upload",
+        headers=headers,
+        data=file,
+    )
+
+    if resp.status_code == 200:
+        LOG.info("Image uploaded successfully")
+        cid = resp.json()["value"]["cid"]
+        link = f"https://{cid}.ipfs.nftstorage.link"
+        LOG.info(f"Link to image is {link}")
+        return link
+    else:
+        err = f"File upload failed with status code {resp.status_code}"
+        LOG.exception(err)
+        raise Exception(err)

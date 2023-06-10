0c60b68c7faeb48f06f7985da17f9fa60ed4899a
ChinaPancake <mateusz.placek99@gmail.com>
2023-06-10 02:49:00 +0200

    working 0.2

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
 from loguru import logger as LOG
 
 from upload_on_ipfs import upload_file_on_ipfs
-from supabase_connection import insert_db, create_user, log_in
-
+from supabase_connection import insert_db, create_user, log_in, LoginResponse, get_events
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "*",
+    "http://localhost:19000/",
+    "ticketex2-production.up.railway.app",
+    "https://ticketex2-production.up.railway.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 class Event(BaseModel):
     name: str
@@ -15,6 +34,9 @@ class Event(BaseModel):
     finish_day: str
     event_image: str
     preview_image: str
+    contract_address: str
+    ticket_quantity:int
+    ticket_price:float
 
 class User(BaseModel):
     user_email: str
@@ -22,29 +44,31 @@ class User(BaseModel):
     user_password: str
 
 # POST
-@app.post("/event")
+@app.post("/event", response_model=dict)
 async def create_event(event: Event):
     LOG.info(f'{insert_db(event_info=event.dict())}')
     return {"git":"good"}
 
-@app.post("/image")
+@app.post("/image", response_model=str)
 def create_picture(second_art: UploadFile = File(...)):
     """Takes image file, returns url to IPFS Storage"""
-
-    LOG.info(f"New NFT was initiated")
     ipfs_url = upload_file_on_ipfs(second_art.file)
-    LOG.info(f"IPFS NTF url: {ipfs_url}")
-    return {"image_url": ipfs_url}
+    return ipfs_url
 
-@app.post("/account")
+@app.post("/account", response_model=LoginResponse)
 def crete_account(user: User):
     return create_user(user = user.dict())
     
 
-@app.post("/login")
+@app.post("/login", response_model=LoginResponse)
 def log_to_account(user_email:str, user_password:str):
     return log_in(user_email=user_email, user_password=user_password)
-    
+
+
+@app.get("/event")
+def get_event():
+    return get_events()
+
 @app.get("/")
 def root():
     return {'working':'hard'}
--- supabase_connection.py
+++ supabase_connection.py
@@ -3,6 +3,11 @@ from supabase import create_client, Client
 from config import PROJECT_URL, PUBLIC_API
 from user_security import hashpassword, generate_jwt_token
 
+from pydantic import BaseModel
+
+class LoginResponse(BaseModel):
+    accessToken: str
+
 def insert_db(event_info:dict):
     url: str = PROJECT_URL
     key: str = PUBLIC_API
@@ -28,12 +33,23 @@ def fetch_user(user_email:str):
     response = supabase.table("user_management").select("*").eq("user_email",f"{user_email}").execute()
     return response
 
-def log_in(user_email:str, user_password:str):
+def log_in(user_email:str, user_password:str) -> LoginResponse :
     for data in fetch_user(user_email=user_email):
-        print(data)
         if type(data[1]) == list:
             print(hashpassword(user_password))
             if data[1][0]['user_password'] == None:
                 continue
+            # TODO
+            # Jeżeli nie ma użytkownika, lub użytkownik wpisał złe hasło, return error bad password or user not found
             elif data[1][0]['user_password'] == hashpassword(user_password):
                return {"accessToken": f"{generate_jwt_token(user_email)}"}
+            else:
+                raise TypeError()
+            
+def get_events():
+    url: str = PROJECT_URL
+    key: str = PUBLIC_API
+    supabase: Client = create_client(url, key)
+
+    response = supabase.table("events").select("*").execute()
+    return response
